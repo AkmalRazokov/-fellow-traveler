23ffc8530a1a62c2e06453a18c2eee214e7c9ba0
AkmalRazokov <rmaks8048@gmail.com>
2025-10-06 10:26:28 +0500

    update

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ app.add_middleware(AdvancedMiddleware)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8003, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8008, reload=True)
